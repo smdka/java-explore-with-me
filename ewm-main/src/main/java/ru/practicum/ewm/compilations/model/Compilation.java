7fb446639aeff29df2e3b70d301e27922e5c7882
smdka <Smdkaalaska@yandex.ru>
2023-05-02 13:08:01 +0300

    <feat> minor fixes

--- ewm-main/src/main/java/ru/practicum/ewm/compilations/model/Compilation.java
+++ ewm-main/src/main/java/ru/practicum/ewm/compilations/model/Compilation.java
@@ -1,10 +1,24 @@
 package ru.practicum.ewm.compilations.model;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import ru.practicum.ewm.events.model.Event;
 
-import javax.persistence.*;
-import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.NamedAttributeNode;
+import javax.persistence.NamedEntityGraph;
+import javax.persistence.NamedSubgraph;
+import javax.persistence.Table;
 import java.util.Set;
 
 @Entity
