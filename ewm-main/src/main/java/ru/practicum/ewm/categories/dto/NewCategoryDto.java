2c61e9e667a419eaac0595d65315bfdb2a26df23
smdka <Smdkaalaska@yandex.ru>
2023-05-02 15:21:13 +0300

    <fix> minor fixes

--- ewm-main/src/main/java/ru/practicum/ewm/categories/dto/NewCategoryDto.java
+++ ewm-main/src/main/java/ru/practicum/ewm/categories/dto/NewCategoryDto.java
@@ -1,10 +1,13 @@
 package ru.practicum.ewm.categories.dto;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 import lombok.Value;
 
 import javax.validation.constraints.NotBlank;
 
 @Value
+@NoArgsConstructor(access = AccessLevel.PRIVATE, force = true)
 public class NewCategoryDto {
     @NotBlank
     String name;
