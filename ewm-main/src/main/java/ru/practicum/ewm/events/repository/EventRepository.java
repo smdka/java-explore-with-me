a709dcc11c4b6b996e447e40c9f4d94e14c76f39
Илья <Smdkaalaska@yandex.ru>
2023-05-04 01:46:23 +0300

    <fix> CategoryServiceImpl.delete fix

--- ewm-main/src/main/java/ru/practicum/ewm/events/repository/EventRepository.java
+++ ewm-main/src/main/java/ru/practicum/ewm/events/repository/EventRepository.java
@@ -57,5 +57,6 @@ public interface EventRepository extends JpaRepository<Event, Long> {
     Optional<Event> findEventByIdAndState(Long eventId, State state);
 
     @EntityGraph(value = "event")
-    boolean existsByCategoryId(Long categoryId);
+    @Query("SELECT CASE WHEN COUNT(e) > 0 THEN TRUE ELSE FALSE END FROM Event e WHERE e.category.id = :categoryId")
+    boolean existsByCategoryId(@Param("categoryId") Long categoryId);
 }
