ac5b0cd4de7e7db230f277b64bcbab6ac57c26cf
Илья <Smdkaalaska@yandex.ru>
2023-05-08 01:27:42 +0300

    <fix> added EntityGraph annotation to overridden CommentRepository.findById()

--- ewm-main/src/main/java/ru/practicum/ewm/comments/repository/CommentRepository.java
+++ ewm-main/src/main/java/ru/practicum/ewm/comments/repository/CommentRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import ru.practicum.ewm.comments.model.Comment;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     @EntityGraph("comment_entity_graph")
@@ -13,4 +14,8 @@ public interface CommentRepository extends JpaRepository<Comment, Long> {
 
     @EntityGraph("comment_entity_graph")
     List<Comment> findAllByEventId(Long eventId, PageRequest page);
+
+    @Override
+    @EntityGraph("comment_entity_graph")
+    Optional<Comment> findById(Long id);
 }
