bc03c6ea50195531ba01b3260a39d41897ccbdb8
smdka <Smdkaalaska@yandex.ru>
2023-06-29 14:30:47 +0300

    <feat> Spring Cloud Config added for ewm-main and ewm-statistic

--- ewm-main/src/main/java/ru/practicum/ewm/ExploreWithMeMain.java
+++ ewm-main/src/main/java/ru/practicum/ewm/ExploreWithMeMain.java
@@ -3,6 +3,7 @@ package ru.practicum.ewm;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@RefreshScope
 @SpringBootApplication
 public class ExploreWithMeMain {
     public static void main(String[] args) {
