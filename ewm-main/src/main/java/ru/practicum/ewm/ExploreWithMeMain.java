8265d161c08a8234b409370e9a35fbf9b8d93ba4
Ilia <Smdkaalaska@yandex.ru>
2023-06-29 19:02:14 +0300

    <fix> dependencies management improvement

--- ewm-main/src/main/java/ru/practicum/ewm/ExploreWithMeMain.java
+++ ewm-main/src/main/java/ru/practicum/ewm/ExploreWithMeMain.java
@@ -2,6 +2,7 @@ package ru.practicum.ewm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @RefreshScope
 @SpringBootApplication
