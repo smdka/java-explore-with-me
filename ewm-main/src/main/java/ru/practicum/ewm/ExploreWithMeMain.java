3ef8af46fe05d6c9a263eba305a6665c4cd0a31d
smdka <Smdkaalaska@yandex.ru>
2023-07-11 16:25:29 +0300

    <feat> adding Spring Stream support (Kafka)

--- ewm-main/src/main/java/ru/practicum/ewm/ExploreWithMeMain.java
+++ ewm-main/src/main/java/ru/practicum/ewm/ExploreWithMeMain.java
@@ -3,8 +3,10 @@ package ru.practicum.ewm;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.stream.annotation.EnableBinding;
 
 @RefreshScope
+@EnableBinding(Source.class) //TODO привести в соответствие с актуальной версией
 @SpringBootApplication
 public class ExploreWithMeMain {
     public static void main(String[] args) {
